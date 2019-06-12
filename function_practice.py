19f17cb791d327b472c35d23bce646496340af61
Skylar Schmidt <skylarschmidt2020@gmail.com>
2019-06-12 11:47:20 -0500

    Added funtion practice

---
+++ function_practice.py
@@ -0,0 +1,19 @@
+"""
+title: function_practice
+author: Skylar
+date: 2019-06-12 11:23
+"""
+
+
+def age_calculator(current_year, birth_year):
+    return current_year - birth_year
+
+
+print(age_calculator(2019, 2002))
+
+
+def avg_numbers(num1, num2, num3):
+    return (num1+num2+num3)/3
+
+
+print(avg_numbers(4, 7, 8))
