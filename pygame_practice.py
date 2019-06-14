30102623de0b48597cccb2292771e5f08abdcfb2
Skylar Schmidt <skylarschmidt2020@gmail.com>
2019-06-14 14:21:02 -0500

    added ball person

--- pygame_practice.py
+++ pygame_practice.py
@@ -6,6 +6,14 @@ date: 2019-06-14 09:48
 
 import pygame
 
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+PI = 3.141592653
 pygame.init()
 
 size = (400, 500)
@@ -17,34 +25,67 @@ pygame.display.set_caption("First Pygame")
 done = False
 clock = pygame.time.Clock()
 
+
+x_speed = 0
+y_speed = 0
+
+x_cord = 10
+y_cord = 10
+
+
+ball_pos = 0
+ball_change = 1
+
+
+def draw_stick_man(screen, x, y):
+    pygame.draw.ellipse(screen, RED, [120 + x, 150 + ball_pos + y, 40, 40])
+    pygame.draw.circle(screen, GREEN, [200 + x, 100 + y], 50)
+
+    pygame.draw.ellipse(screen, BLACK, [170 + x, 80 + y, 14, 17])
+    pygame.draw.ellipse(screen, BLACK, [210 + x, 80 + y, 14, 17])
+
+    pygame.draw.arc(screen, BLACK, [160 + x, 75 + y, 75, 62], PI, 0, 2)
+    pygame.draw.arc(screen, BLACK, [160 + x, 75 + y, 75, 62], PI, PI, 2)
+
+    pygame.draw.line(screen, GREEN, [200 + x, 150 + y], [200 + x, 300 + y], 2)
+    pygame.draw.line(screen, GREEN, [200 + x, 300 + y], [150 + x, 340 + y], 2)
+    pygame.draw.line(screen, GREEN, [200 + x, 300 + y], [250 + x, 340 + y], 2)
+    pygame.draw.line(screen, GREEN, [200 + x, 200 + y], [150 + x, 150 + y], 2)
+    pygame.draw.line(screen, GREEN, [200 + x, 200 + y], [250 + x, 150 + y], 2)
+
+
 # Loop as long as done == False
 while not done:
+    ball_pos += ball_change
+
+    if ball_pos > 150:
+        ball_change -= 1
+    elif ball_pos < 20:
+        ball_change += 1
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x_speed = -3
+    if keys[pygame.K_RIGHT]:
+        x_speed = 3
+    if keys[pygame.K_UP]:
+        y_speed = -3
+    if keys[pygame.K_DOWN]:
+        y_speed = 3
 
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
 
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    BLUE = (0, 0, 255)
-    GREEN = (0, 255, 0)
-    RED = (255, 0, 0)
+    x_cord += x_speed
+    y_cord += y_speed
 
-    PI = 3.141592653
+    x_speed = 0
+    y_speed = 0
 
-    pygame.draw.circle(screen, GREEN, [200, 100], 50)
+    screen.fill(BLACK)
 
-    pygame.draw.ellipse(screen, BLACK, [170, 80, 14, 17])
-    pygame.draw.ellipse(screen, BLACK, [210, 80, 14, 17])
-
-    pygame.draw.arc(screen, BLACK, [160, 75, 75, 62], PI, 0, 2)
-    pygame.draw.arc(screen, BLACK, [160, 75, 75, 62], PI, PI, 2)
-
-    pygame.draw.line(screen, GREEN, [200, 150], [200, 300], 2)
-    pygame.draw.line(screen, GREEN, [200, 300], [150, 340], 2)
-    pygame.draw.line(screen, GREEN, [200, 300], [250, 340], 2)
-    pygame.draw.line(screen, GREEN, [200, 200], [150, 150], 2)
-    pygame.draw.line(screen, GREEN, [200, 200], [250, 150], 2)
+    draw_stick_man(screen, x_cord, y_cord)
 
     # Go ahead and update the screen with what we've drawn.
     # This MUST happen after all the other drawing commands.
